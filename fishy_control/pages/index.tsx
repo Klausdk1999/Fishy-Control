16342f86e375cf65fe40572e3cb4b8fc45c35f95
Klausdk1999 <klausdk1999@gmail.com>
2022-11-22 11:54:52 -0300

    dinamic update dash

--- fishy_control/pages/index.tsx
+++ fishy_control/pages/index.tsx
@@ -3,19 +3,63 @@ import { Card } from '@mui/material';
 import { Stack } from '@mui/system';
 import Menu from '../components/menu'
 import useSwr from 'swr';
+import axios from 'axios';
 import CircularIndeterminate from '../components/CircularIndeterminate';
+
 export type requestData = {
     Temperature: number,
     Output: number
 };
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json())
+let tempOut:requestData;
 
 export default function Dashboard() {
-  const { data, error } = useSwr<requestData>('/api/data', fetcher,{ refreshInterval: 1000 })
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [temp, setTemp] = React.useState(0);
+  const [out, setOut] = React.useState(0);
+  
+  async function getData() {
+    try {
+      // 👇️ const data: GetUsersResponse
+      const { data, status } = await axios.get<requestData>(
+        'https://fishy-control.vercel.app/api/data',
+        {
+          headers: {
+            Accept: 'application/json',
+          },
+        },
+      );
+  
+      console.log(JSON.stringify(data, null, 4));
+  
+      // 👇️ "response status is: 200"
+      console.log('response status is: ', status);
+  
+      tempOut = data;
+      setTemp(tempOut.Temperature);
+      setOut(tempOut.Output);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.log('error message: ', error.message);
+        return error.message;
+      } else {
+        console.log('unexpected error: ', error);
+        return 'An unexpected error occurred';
+      }
+    }
+  }
 
-  if (error) return <div>Failed to load data</div>
-  if (!data) return <CircularIndeterminate/>
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      getData();
+      
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
+  //if (error) return <div>Failed to load data</div>
+  if (isLoading) return <CircularIndeterminate/>
 
   return (
     <>
@@ -50,10 +94,10 @@ export default function Dashboard() {
       spacing={3}
       >
         <Card sx={{ fontSize: 25,textAlign: 'center', fontWeight: 700,width: 200, height:70 , p:2 , m:2}}>
-          {data.Temperature} C
+          {temp} C
         </Card>
         <Card sx={{ fontSize: 25,textAlign: 'center', fontWeight: 700,width: 200, height:70 , p:2 , m:2}}>
-          {data.Output}
+          {out}
         </Card>
       </Stack>
     </Stack>  
