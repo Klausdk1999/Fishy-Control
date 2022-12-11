b3eb1f84176076aac34291aa3682375b84fa9fd3
Klausdk1999 <klausdk1999@gmail.com>
2022-12-10 21:24:38 -0300

    fix space

--- fishy_control/pages/index.tsx
+++ fishy_control/pages/index.tsx
@@ -55,10 +55,10 @@ export default function Dashboard() {
       alignItems="center"
       spacing={3}
       >
-        <Card sx={{ fontSize: 25,textAlign: 'center', fontWeight: 700,width: 200, height:70 , p:2 , m:2}}>
-          {data.Temperature!=0 ? <>{data.Temperature} +- 0.25 °C </> : <><CircularIndeterminate/></>} 
+        <Card sx={{ fontSize: 25,textAlign: 'center', fontWeight: 700,width: 220, height:70 , p:2 , m:2}}>
+          {data.Temperature!=0 ? <>{data.Temperature} +-0.25 °C </> : <><CircularIndeterminate/></>} 
         </Card>
-        <Card sx={{ fontSize: 25,textAlign: 'center', fontWeight: 700,width: 200, height:70 , p:2 , m:2}}>
+        <Card sx={{ fontSize: 25,textAlign: 'center', fontWeight: 700,width: 220, height:70 , p:2 , m:2}}>
           {data.Temperature!=0 ? <>{data.Output}</> : <><CircularIndeterminate/></>}
         </Card>
       </Stack>
